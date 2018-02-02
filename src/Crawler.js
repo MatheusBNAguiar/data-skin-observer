7cc82a31d1c68051af16bf532c2ce3389e566130
Matheus Aguiar <matheus.bnaguiar@gmail.com>
2018-02-02 19:08:21 -0200

    Refactor Crawler

---
+++ src/Crawler.js
@@ -0,0 +1,39 @@
+const { getData } = require('./Fetcher');
+const { writeJson } = require('./Json');
+
+const crawlDataSkins = pageData => {
+    const dataSkins = pageData.match(/\.addTheme\((([a-zA-Z0-9_."]*(,[a-zA-Z0-9_\-."]*)*)*)./g)
+        .map(a => a.replace(/\.addTheme\(|\)|"/g, ''));
+    return dataSkins;
+};
+
+const getObjectFromList = dataSkins => {
+    const dataSkinObject = {};
+    dataSkins.forEach(skin => {
+        const [widget, dataSkin] = skin.split(',');
+        const dataSkinName = dataSkin === 'null' ? 'default' : dataSkin;
+        dataSkinObject[widget] = dataSkinObject[widget] || {};
+        dataSkinObject[widget][dataSkinName] = { enabled: false };
+    });
+    return dataSkinObject;
+};
+
+module.exports = {
+    crawlDataSkin: apiKey => {
+        const currentUrl = `https://static.chaordicsystems.com/static/${apiKey}/current.js`;
+        getData(currentUrl)
+            .then(response => response.text())
+            .then(textData => crawlDataSkins(textData))
+            .then(dataSkins => getObjectFromList(dataSkins))
+            .then(dataSkinObject => {
+                const dataPath = `./objects/${apiKey}.json`;
+                writeJson(dataPath, dataSkinObject);
+                return dataSkinObject;
+            })
+            .then(dataSkinObject => {
+                console.log(`[Crawler] ${apiKey} saved`);
+                return dataSkinObject;
+            })
+            .catch(err => console.log(err));
+    },
+};
