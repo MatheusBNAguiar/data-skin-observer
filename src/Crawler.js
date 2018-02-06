eeb1fc5a083651f7946f0497a5b07ad0fa659b9d
Matheus Aguiar <matheus.bnaguiar@gmail.com>
2018-02-06 12:00:05 -0200

    Refactor code to get when there is a var inside the declaration of .addTheme

--- src/Crawler.js
+++ src/Crawler.js
@@ -6,13 +6,19 @@ const log = require('node-pretty-log');
 const crawlDataSkins = pageData => {
     const dataSkins = pageData.match(/\.addTheme\((([a-zA-Z0-9_."]*(,[a-zA-Z0-9_\-."]*)*)*)./g)
         .map(a => a.replace(/\.addTheme\(|\)|"/g, ''));
-    return dataSkins;
+    return [dataSkins, pageData];
 };
 
-const getObjectFromList = dataSkins => {
+
+const getVariableResult = (variableName, pageData) => pageData.match(new RegExp(`[\\s]${variableName}\\="([a-zA-Z0-9_\\-]*)"`))[1];
+
+const getObjectFromList = (dataSkins, pageData) => {
     const dataSkinObject = {};
     dataSkins.forEach(skin => {
-        const [widget, dataSkin] = skin.split(',');
+        const splittedSkin = skin.split(',');
+        const [widget] = splittedSkin;
+        const dataSkin = splittedSkin[1] &&
+        splittedSkin[1].length > 2 ? splittedSkin[1] : getVariableResult(splittedSkin[1], pageData);
         const dataSkinName = dataSkin === 'null' ? 'default' : dataSkin;
         dataSkinObject[widget] = dataSkinObject[widget] || {};
         dataSkinObject[widget][dataSkinName] = { enabled: false };
@@ -20,13 +26,14 @@ const getObjectFromList = dataSkins => {
     return dataSkinObject;
 };
 
+
 module.exports = {
     crawlDataSkin: apiKey => {
         const currentUrl = `https://static.chaordicsystems.com/static/${apiKey}/current.js`;
         getData(currentUrl)
             .then(response => response.text())
             .then(textData => crawlDataSkins(textData))
-            .then(dataSkins => getObjectFromList(dataSkins))
+            .then(data => getObjectFromList(...data))
             .then(dataSkinObject => {
                 const dataPath = `./objects/${apiKey}.json`;
                 writeJson(dataPath, dataSkinObject);
